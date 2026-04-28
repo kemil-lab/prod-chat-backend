7f7ba15829dfa0c43146a1bc56c65b0bee10defb
Kemil Dhanani <kemil@yarnit.app>
2026-04-28 16:52:06 +0530

    config fixes

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     DB_NAME: str 
     NAME_SPACE: str 
     HUGGINGFACE_API_KEY: str 
-    OPENAI_API_KEY: str
+    # OPENAI_API_KEY: str
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
